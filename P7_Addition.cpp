fce3705a8be77f3b66f953f21de2412da04b33da
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-09-29 10:39:21 +0530

    Updated!

---
+++ P7_Addition.cpp
@@ -0,0 +1,21 @@
+
+#include<iostream>
+
+using namespace std;
+int  main()
+{
+
+int a,b,c; //Variable declaration statement
+a=10;
+b=6;
+c=a+b;
+cout<<"a="<<a;
+cout<<"\nb="<<b;
+cout<<"\nc="<<c;
+
+
+return 0;
+}
+//Variable is defined as a named memory location, its value can change during program
+// \n is newline character. It inserts enter key
+
