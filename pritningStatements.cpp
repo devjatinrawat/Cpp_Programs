8cce352f74d78dbdbb7ae40b24ddb89e71ac6d9b
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-09-29 09:15:35 +0530

    Updated!

---
+++ pritningStatements.cpp
@@ -0,0 +1,16 @@
+
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+
+cout<<7;
+cout<<endl<<45.78;	//endl stands for endline,header file required is iostream.h
+cout<<"\n"<<'%';
+cout<<'\n'<<"JatinRawat";
+
+return 0;
+}
