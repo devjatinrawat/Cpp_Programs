7f2cae99789e3facc6ebc1712b1cd45f4cd78220
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-09-29 09:26:09 +0530

    Updated!

--- variableDec.cpp
+++ variableDec.cpp
@@ -1,15 +1,14 @@
-
-
-#include<iostream.h>
-
-
+#include<iostream>
 using namespace std;
 
-void main()
+int main()
 {
 
 int a;	//variable declaration statement, a is a variable of int type
 a=10;
 cout<<"a="<<a;	//If << appears for multiple times in a line,it is known as cascading of insertion operator
 
+return 0;
 }
+
+
