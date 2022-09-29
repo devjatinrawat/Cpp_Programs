94123e008e82ae0681b0b740bf8407a0de597f63
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-09-29 10:48:49 +0530

    Updated!

---
+++ P10_quadraticEq.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+int main()
+{
+
+int a,b,c;
+float root1,root2;
+cout<<"\nEnter value of a=";
+cin>>a;
+cout<<"\nEnter value of b=";
+cin>>b;
+cout<<"\nEnter value of c=";
+cin>>c;
+root1=(-b+sqrt(b*b-4*a*c))/2*a;
+root2=(-b-sqrt(b*b-4*a*c))/(2*a);
+cout<<"\nRoot1="<<root1;
+cout<<"\nRoot2="<<root2;
+return 0;
+}
