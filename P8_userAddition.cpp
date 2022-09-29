132411ba90d22186c34f9019e026ea8df668cfcb
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-09-29 10:42:33 +0530

    Updated!

---
+++ P8_userAddition.cpp
@@ -0,0 +1,25 @@
+
+//Program to add two numbers
+#include<iostream>
+
+using namespace std;
+int main()
+{
+
+int a,b,c;
+cout<<"\nEnter value of a:=";
+cin>>a;
+cout<<"\nPut value of b:=";
+cin>>b;
+c=a+b;
+cout<<"\na="<<a;
+cout<<"\nb="<<b;
+cout<<"\nsum="<<c;
+
+return 0;
+
+}
+//cout stands for common output, It is used to display something on screen
+//cin stands for common input, it is used to read value of a variable
+//>> is called extraction operator/input operator
+//<< is called insertion operator/Output operator
