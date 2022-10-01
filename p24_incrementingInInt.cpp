159b5edb81a2a494f0b8025ce9f2e26389ac4520
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-01 15:45:37 +0530

    Updated!

---
+++ p24_incrementingInInt.cpp
@@ -0,0 +1,19 @@
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+
+int a=10;
+cout<<endl<<++a;
+cout<<endl<<a++;
+cout<<endl<<a;
+cout<<endl<<--a;
+cout<<endl<<a--;
+cout<<endl<<a ;
+return 0;
+}
