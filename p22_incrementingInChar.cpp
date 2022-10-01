566817cf5227b0eeb961892afff65e47644fd397
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-01 15:41:38 +0530

    Updated!

---
+++ p22_incrementingInChar.cpp
@@ -0,0 +1,27 @@
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+//++ increment operator
+//-- decrement operator
+int main()
+{
+
+char ch='A';
+int b=100;
+cout<<'\n'<<ch;
+ch++;
+cout<<'\n'<<ch;
+
+cout<<'\n'<<(int)ch;
+
+cout<<'\n'<<(char)b;
+
+return 0;
+}
+//ASCII code : American Standard Code for Information Interchange
+//A=65,B=66,...........
+//a=97,b=98,...........
+//0=48,1=49,.....9
