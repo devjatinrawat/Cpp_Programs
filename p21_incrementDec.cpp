feb8437db87d8996be687ce32dd963d841a36f0f
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-01 15:39:57 +0530

    Updated!

---
+++ p21_incrementDec.cpp
@@ -0,0 +1,26 @@
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+//++ increment operator
+//-- decrement operator
+int main()
+{
+
+int a=10;
+cout<<'\n'<<a;
+++a;  //a=a+1;  Prefix form
+cout<<endl<<a;	//endl stands for endline
+a++;		//Postfix form
+cout<<endl<<a;
+a--;
+cout<<"\n"<<a;
+--a;
+cout<<'\n'<<a;
+
+return 0;
+}
+
+
