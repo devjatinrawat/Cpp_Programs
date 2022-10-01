9b8a5f34c710449d3daa076d694ac7526286e4d9
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-01 15:47:17 +0530

    Updated!

---
+++ p25_incrementingInInt.cpp
@@ -0,0 +1,17 @@
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+int main()
+{
+
+int a=10;
+cout<<endl<<++a<<","<<a++;  //Cascading of insertion operator
+cout<<endl<<a--<<","<<--a;
+cout<<endl<<a;
+return 0;
+}
+
+
