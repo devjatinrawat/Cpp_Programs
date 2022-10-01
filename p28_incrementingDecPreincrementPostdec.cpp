87fd58052b5c150cc9d4a847a802e53851cd0a61
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-01 15:53:30 +0530

    Updated!

---
+++ p28_incrementingDecPreincrementPostdec.cpp
@@ -0,0 +1,18 @@
+
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+int a=10;
+cout<<endl<<a++ + ++a;
+cout<<endl<<a;
+cout<<endl<<a-- - --a;
+cout<<endl<<a;
+cout<<endl<<a++ - a++;
+cout<<endl<<a;
+return 0;
+}
