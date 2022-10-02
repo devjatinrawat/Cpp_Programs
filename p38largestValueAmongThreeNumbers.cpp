dc70ae34f979d819dba442cce6cef7b5d394a600
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-02 22:41:45 +0530

    Updated!

---
+++ p38largestValueAmongThreeNumbers.cpp
@@ -0,0 +1,18 @@
+
+/* Largest Numbers Using Ternary Opertors Among 3 Numbers  */
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a,b,c;
+cout<<"\nEnter value of a=";
+cin>>a;
+cout<<"\nEnter value of b=";
+cin>>b;
+c=a>b?a:b;
+cout<<"\nLargest value="<<c;
+return 0;
+}
