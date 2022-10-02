bab9a176189090128a3eec3f676c6895832c03a2
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-02 22:23:38 +0530

    Updated!

---
+++ p36shorthandOperators.cpp
@@ -0,0 +1,23 @@
+
+
+#include<iostream>
+using namespace std;
+
+/*Shorthand operators or assignment operators*/
+int main()
+{
+int a=15;
+a+=10;   //a=a+10
+cout<<"\na+=10 ="<<a;
+a-=3;   //a=a-3
+cout<<"\na-=3 ="<<a;
+a*=5;   //a=a*5
+cout<<"\na*=5 ="<<a;
+a/=10;   //a=a/10
+cout<<"\na/=10 ="<<a;
+a %=3;   //a=a%3
+cout<<"\na%=3 ="<<a;
+return 0;
+}
+
+
