91921517de61e1e7ae384b74a538585bf902fa54
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-02 22:13:41 +0530

    Updated!

---
+++ p33relationalOperators.cpp
@@ -0,0 +1,25 @@
+
+
+#include<iostream>
+using namespace std;
+
+//Relational operators
+int main()
+{
+int a,b;
+cout<<"\nEnter value of a=";
+cin>>a;
+cout<<"\nEnter value of b=";
+cin>>b;
+cout<<"\na<b="<<(a<b);
+cout<<"\na<=b="<<(a<=b);
+cout<<"\na>b="<<(a>b);
+cout<<"\na>=b="<<(a>=b);
+cout<<"\na==b="<<(a==b);
+cout<<"\na!=b="<<(a!=b);
+cout<<"\n!a="<<(!a);  //(a==0)
+cout<<"\na="<<(a=b);
+return 0;
+}
+
+
