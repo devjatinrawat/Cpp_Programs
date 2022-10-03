0c7516b0754bc32744becc77e37b947ead3457c9
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-03 09:20:46 +0530

    Updated!

---
+++ p31_sizeOfOperator.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+float b;
+char c;
+cout<<"\nsizeof(a)="<<sizeof(a);
+cout<<"\nsizeof(b)="<<sizeof(b);
+cout<<"\nsizeof(c)="<<sizeof(c);
+cout<<"\nsizeof(double)="<<sizeof(double);
+cout<<"\nsizeof(long double)="<<sizeof(long double);
+cout<<"\nsizeof(short)="<<sizeof(short);
+cout<<"\nsizeof(123)="<<sizeof(123);
+cout<<"\nsizeof(123l)="<<sizeof(123l);
+cout<<"\nsizeof(123.55)="<<sizeof(123.55);
+cout<<"\nsizeof(123.55f)="<<sizeof(123.55f);
+cout<<"\nsizeof(123.55L)="<<sizeof(123.55L);
+
+return 0;
+}
+
+// Continue From Here!
