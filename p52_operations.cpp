2ca4db335da68be2a1caad01a929e4b5ce929c90
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-04 09:15:55 +0530

    Updated!

---
+++ p52_operations.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+float a;
+a=pow(2,5);
+cout<<endl<<a;
+a=sqrt(5);
+cout<<endl<<a;
+a=sin(65);
+cout<<endl<<a;
+a=cos(65);
+cout<<endl<<a;
+a=tan(65);
+cout<<endl<<a;
+a=log10(65);
+cout<<endl<<a;
+a=exp(65);       //Antilog
+cout<<endl<<a;
+a=ceil(65.4);
+cout<<endl<<a;
+a=floor(65.4);
+cout<<endl<<a;
+a=abs(-5);
+cout<<endl<<a;
+a=fabs(-5.98);
+cout<<endl<<a;
+return 0;
+}
