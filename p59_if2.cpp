6acb7e50e1e8338d79c795af71987343802a139b
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-04 09:36:59 +0530

    Updated!

---
+++ p59_if2.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter value of a=";
+cin>>a;
+if(a>0)
+{
+cout<<"\na is positive";
+cout<<"\nWelcome";
+}
+cout<<"\nBye...";
+return 0;
+
+}
