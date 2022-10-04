759a3e370c308203c60dffef86c04e4c9a8e12a7
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-04 09:18:44 +0530

    Updated!

---
+++ p54_dataTypes.cpp
@@ -0,0 +1,21 @@
+
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+double b;
+char c;
+cout<<"\n value of a=";
+cin>>a;
+cout<<"\n value of b=";
+cin>>b;
+cout<<"\n value of c=";
+cin>>c;
+cout<<"\na="<<a;
+cout<<"\nb="<<b;
+cout<<"\nc="<<c;
+return 0;
+}
