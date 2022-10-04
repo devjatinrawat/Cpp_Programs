bb235e06dbf4ab03b2f0c00801787205d065574c
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-04 09:38:13 +0530

    Updated!

---
+++ p60_if3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a;
+cout<<"\nEnter value of a:=";
+cin>>a;		//>> is called input operator or extraction operator
+if(a>0)
+cout<<"\nNumber is +ve";
+if(a<0)
+cout<<"\nNumber is -ve";
+if(a==0)
+cout<<"\nNumber is zero";
+return 0;
+}
