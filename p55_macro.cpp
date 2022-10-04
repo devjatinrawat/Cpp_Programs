cfe3f1a7e75cb58b7b905bc80f0b83a7947faebf
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-04 09:29:09 +0530

    Updated!

---
+++ p55_macro.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#define pi 3.141     //Symbolic constant
+#define area(x) x*x;  //Macro
+
+using namespace std;
+
+int main()
+{
+cout<<endl<<pi;
+cout<<endl<<area(5);
+return 0;
+}
