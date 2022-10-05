bec4958aa0895a73cd7a5e6b59f7580712ca468f
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 12:14:00 +0530

    Updated!

---
+++ p70_if13.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+
+{
+int a,b;
+cout<<"\nEnter two numbers:=";
+cin>>a>>b;
+if(a>b)
+cout<<"\na is larger than b";
+else
+cout<<"\na is not larger than b";
+return 0;
+}
