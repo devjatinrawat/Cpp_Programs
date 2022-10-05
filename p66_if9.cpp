7db12bf5c6fb933efc527ae0cd3d154204c31f1b
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 12:09:21 +0530

    Updated!

---
+++ p66_if9.cpp
@@ -0,0 +1,17 @@
+
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter a number:=";
+cin>>a;
+if(a>0)
+cout<<"\nnumber is +ve";
+else
+cout<<"\nNumber isn't +ve";
+cout<<"\nBye";
+return 0;
+}
