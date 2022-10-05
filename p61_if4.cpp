f1f76db8f8ccc283cdfdff5c97418951b2e1ff8a
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 09:37:38 +0530

    Updated!

---
+++ p61_if4.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+//Simple if
+int main()
+{
+int a,b;
+cout<<"\nEnter first number:=";
+cin>>a;
+cout<<"\nEnter second number:=";
+cin>>b;
+if(a>b)
+cout<<endl<<"a is largest";
+if(b>a)
+cout<<endl<<"b is largest";
+if(a==b)
+cout<<endl<<"Both values are same";
+
+return 0;
+}
+// continue From Here!
