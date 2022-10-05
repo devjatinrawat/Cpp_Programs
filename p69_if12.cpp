34a2ea5c8005b2bc18694a0303ba11e42e17c02f
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 12:12:58 +0530

    Updated!

---
+++ p69_if12.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter a number:=";
+cin>>a;
+if(a%5==0)
+cout<<"\nnumber is a multiple of 5";
+else
+cout<<"\nNumber is not a multiple of 5";
+return 0;
+
+
+}
