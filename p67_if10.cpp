493a01ccec383914eeb165d1a699c49aff714697
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 12:10:39 +0530

    Updated!

---
+++ p67_if10.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter a number:=";
+cin>>a;
+if(a>0)
+{
+cout<<"\nnumber is +ve";
+cout<<"\nWelcome";
+}
+else
+{
+cout<<"\nNumber isn't +ve";
+cout<<"\nBye";
+}
+return 0;
+}
