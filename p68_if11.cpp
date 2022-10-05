36f95fe55777ca31783033210542392786de00fa
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-05 12:11:49 +0530

    Updated!

---
+++ p68_if11.cpp
@@ -0,0 +1,17 @@
+//Program to check whether a number is even or odd
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter a number=";
+cin>>a;
+if(a%2==0)
+cout<<"\nnumber is even";
+else
+cout<<"\nNumber is odd";
+
+return 0;
+}
