985348311f7bf48f0f0eebfb6856e749d6a13eba
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-06 16:55:25 +0530

    Updated!

---
+++ p76_if19.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+
+int main()
+{
+int a,b;
+cout<<"\nEnter first value :=";
+cin>>a;
+cout<<"\nEnter second value :=";
+cin>>b;
+if(a>b)
+cout<<"\na>b";
+else if(b>a)
+cout<<"\nb>a";
+else
+cout<<"\na & b are equal";
+return 0;
+}
