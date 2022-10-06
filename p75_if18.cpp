260d91308338ef2415f330e212e0329ba23294bc
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-10-06 16:53:47 +0530

    Updated!

---
+++ p75_if18.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main()
+{
+int a;
+cout<<"\nEnter a value :=";
+cin>>a;
+if(a>0)
+cout<<"\nValue is positive";
+else if(a<0)
+cout<<"\nValue is negative";
+else
+cout<<"\nValue is zero";
+return 0;
+}
+
