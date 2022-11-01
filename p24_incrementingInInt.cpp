94329ef1adca323ea0a905027680006e35a1b6a6
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-01 18:24:19 +0530

    Updated!

--- p24_incrementingInInt.cpp
+++ p24_incrementingInInt.cpp
@@ -9,11 +9,11 @@ int main()
 {
 
 int a=10;
-cout<<endl<<++a;
-cout<<endl<<a++;
-cout<<endl<<a;
-cout<<endl<<--a;
-cout<<endl<<a--;
-cout<<endl<<a ;
+cout<<endl<<++a;   // 11
+cout<<endl<<a++;    //11
+cout<<endl<<a;      //12
+cout<<endl<<--a;    //11
+cout<<endl<<a--;  // 11
+cout<<endl<<a ;   //10
 return 0;
 }
