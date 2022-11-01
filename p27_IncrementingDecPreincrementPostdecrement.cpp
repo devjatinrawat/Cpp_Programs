f6a0d7c58f0dfe05d9db2b4b7f2577d57f18b2ad
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-01 18:32:30 +0530

    Updated!

--- p27_IncrementingDecPreincrementPostdecrement.cpp
+++ p27_IncrementingDecPreincrementPostdecrement.cpp
@@ -1,5 +1,21 @@
 
+// Adding Small Piece of Code 
 
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+int a= 1;  
+cout<<endl<<a++ + ++a;
+
+
+return 0;
+}
+
+// Another Code 
 #include<iostream>
 #include<conio.h>
 
