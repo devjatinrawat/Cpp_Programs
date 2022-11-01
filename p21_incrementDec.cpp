987a27310bb589bd6d65be83aa9da074c8298b15
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-01 18:11:28 +0530

    Updated!

--- p21_incrementDec.cpp
+++ p21_incrementDec.cpp
@@ -11,14 +11,18 @@ int main()
 
 int a=10;
 cout<<'\n'<<a;
-++a;  //a=a+1;  Prefix form
-cout<<endl<<a;	//endl stands for endline
-a++;		//Postfix form
-cout<<endl<<a;
-a--;
-cout<<"\n"<<a;
---a;
-cout<<'\n'<<a;
+++a;    
+/* 
+Here first the value is incremented and then printed .
+a = 11
+*/  
+cout<<endl<<a;	//endl stands for endline ,11
+a++;		//here first the value is used , 11 and then incremented 
+cout<<endl<<a; // 12 incremented 
+a--;  // 12 is used first then dec
+cout<<"\n"<<a;  // 11
+--a; // first dec means 11-1 = 10
+cout<<'\n'<<a; // 10
 
 return 0;
 }
