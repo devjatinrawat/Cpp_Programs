19c9b39798674d1cb91e6f282305c2e4aac2ee3b
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-01 19:05:01 +0530

    Updated!

--- p28_incrementingDecPreincrementPostdec.cpp
+++ p28_incrementingDecPreincrementPostdec.cpp
@@ -1,4 +1,34 @@
 
+// Another Code 
+
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+int main()
+{
+int i = 1;
+int j = 2;
+int k;
+
+k = i + j + i++ + j++ + ++i + ++j;
+/* Move From L To R 
+
+i is 1 +
+j is 2 + 
+use 1 
+increment 1 to 2 + 
++ use 2 increment j to 3
++ use 3 + use 3 
+
+*/  
+cout<<i<<" "<<j<<" "<<k<<" ";
+return 0;
+}
+
+
+
 
 #include<iostream>
 #include<conio.h>
