4c8d3928e529613a0beba1635ab43566dd6b9bd3
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-02 22:32:17 +0530

    Updated!

---
+++ p101_while01.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+void main()
+{
+
+int i;
+i=1; //initialization
+while(i<=5) //condition
+{
+cout<<endl<<i;
+i++;  //updation
+}
+getch();
+}
