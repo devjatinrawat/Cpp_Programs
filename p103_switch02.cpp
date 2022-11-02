5700aafb8b3fc7e431985f4bed1ce9a8d1df0501
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-02 22:34:16 +0530

    Updated!

--- p103_switch02.cpp
+++ p103_switch02.cpp
@@ -6,27 +6,12 @@ using namespace std;
 void main()
 {
 
-int choice;
-int a=10;
-cout<<"\n1. Case 1";
-cout<<"\n2. Case 2";
-cout<<"\nEnter your choice:-";
-cin>>choice;
-switch(choice+a)
+int i;
+i=2;
+while(i<=6)
 {
-case 11:
-		cout<<"\nCase 1 selected";
-		break;
-
-case 11+1:
-		cout<<"\nCase 2 selected";
-		break;
-default:
-	cout<<"Wrong choice";
+cout<<endl<<i;
+i+=2;  //i=i+2
 }
 getch();
 }
-
-
-
-
