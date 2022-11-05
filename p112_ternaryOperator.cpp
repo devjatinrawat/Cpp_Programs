e15225ff9f7187feb5925a122be62aa22a6aaacc
Jatin Rawat <111337427+devjatinrawat@users.noreply.github.com>
2022-11-05 22:12:09 +0530

    Updated!

---
+++ p112_ternaryOperator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<conio.h>
+
+using namespace std;
+
+//ternary operator or conditional operator?:
+void main()
+{
+int a=15,b=5,c;
+c=a>b?a:b;
+cout<<"\nc="<<c;
+getch();
+}
